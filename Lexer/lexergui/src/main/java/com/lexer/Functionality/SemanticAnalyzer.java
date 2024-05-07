4d2fd7ac75998b54f649c99460b1e8873cd6e554
Jess <145494578+Jessica-Isunza@users.noreply.github.com>
2024-05-07 09:10:48 -0600

    cube

--- Lexer/lexergui/src/main/java/com/lexer/Functionality/SemanticAnalyzer.java
+++ Lexer/lexergui/src/main/java/com/lexer/Functionality/SemanticAnalyzer.java
@@ -10,6 +10,74 @@ import java.util.Vector;
 public class SemanticAnalyzer {
     private static Hashtable<String, Vector<SymbolTableItem>> symbolTable = new Hashtable<>();
     public static ErrorHandler errorHandler = new ErrorHandler("SEMANTIC ANALYZER");
+    private static final Stack stack;
+    
+    int cube [][][]
+
+    public static int OP_Plus = 1;
+    public static int OP_Minus = 2;
+    public static int OP_Mult = 3;
+    public static int OP_Division = 4;
+    public static int OP_And = 5;
+    public static int OP_Or = 6;
+    public static int OP_Not = 7;
+    public static int OP_Minor = 8;
+    public static int OP_Greater = 9;
+    public static int OP_Equal = 10;
+    public static int OP_LorEqual = 11;
+    public static int OP_GorEqual = 12;
+    public static int OP_Different = 13;
+    public static int OP_Reminder = 14;
+    public static int OP_Assignation = 15;
+
+
+
+    public static int INTEGER = 1;
+    public static int FLOAT = 2;
+    public static char CHAR = 3;
+    public static String STRING = ;
+    public static bool BOOLEAN = ;
+    public static void VOID = ;
+
+
+
+    cube[OP_Plus][INTEGER][INTERGER] = INTEGER;
+    cube[OP_Plus][FLOAT][INTERGER] = FLOAT;
+    cube[OP_Plus][INTEGER][FLOAT] = FLOAT;
+
+
+    private static final Hashtable<String, Vector<SymbolTableItem>> getSymbolTable();
+
+    public static void checkVariable(Srting type, String id)
+
+    public static String isBoolean()
+
+    public static String isTypeMatching()
+
+    public static void stackPush(String type)
+
+    public static String stackPop()
+
+    private String calculateType(String type, String operator)
+
+    private String calculateType(String type1, String type2, String operator)
+
+    private static void error(Gui gui, int error, int line, String info)
+
+
+
+
+    /*public static GlobalObject globalObject = new GlobalObject();
+
+    public static class GlobalObject {
+        //propiedades y metodos
+        public int currentToken;
+        public int property2;
+
+        public GlobalObject() {
+            // Constructor de los objetos
+        }
+    }*/
 
     public static void CheckVariable(String type, String id){
         Vector<SymbolTableItem> items = symbolTable.get(id);
